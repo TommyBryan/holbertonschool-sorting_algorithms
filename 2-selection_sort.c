ff55a352610e3de840866472767956fe017f2d38
TommyBryan <tommy0713@gmail.com>
2024-12-15 17:54:31 -0400

    Made file 2-selection_sort.c betty style format

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,27 +8,27 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_index;
-    int temp;
+	size_t i, j, min_index;
+	int temp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_index = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_index])
-                min_index = j;
-        }
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
 
-        if (min_index != i)
-        {
-            temp = array[i];
-            array[i] = array[min_index];
-            array[min_index] = temp;
-            print_array(array, size);  /* Print array after each swap */
-        }
-    }
+		if (min_index != i)
+		{
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
+			print_array(array, size);  /* Print array after each swap */
+		}
+	}
 }
